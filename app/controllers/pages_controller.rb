41c1a0d1195b981559c43e95a5e6427852cbcd25
Nawaz Ali <mir_nizam@hotmail.com>
2015-06-22 14:29:16 +0000

    Improved About page link and added careers page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,4 +4,7 @@ class PagesController < ApplicationController
   
   def about
   end
+  
+  def careers
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get '/careers' => 'pages#careers'
   get '/about' => 'pages#about'
   root 'pages#home'
   # The priority is based upon order of creation: first created -> highest priority.
